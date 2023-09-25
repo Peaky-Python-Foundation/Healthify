be6f8d9e3ed67a054e48a918c03ae2f31ce954cf
Harsh <harshrawat096@gmail.com>
2023-09-25 20:06:38 +0530

    SignUp ui is completed!

--- healthify/urls.py
+++ healthify/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,3 +28,8 @@ urlpatterns = [
     # Account App
     path('',include('accounts.urls')),
 ]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
